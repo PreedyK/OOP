64883d42fca8ffba7899640923a10a23fb520cdd
PreedyK <kiryn.preedy@gmail.com>
2025-09-26 09:30:33 -0500

    Add files via upload

---
+++ cw_6-3.py
@@ -0,0 +1,16 @@
+myList=[]
+a=10
+
+def function1(r):
+    r = r+1
+    area = 2*3.14*r
+    return area
+
+def function2(a):
+    a = 50
+    b = "Justus"
+
+while True:
+    radius = 10
+    calculated_area = function1(radius)
+    print(calculated_area)
