428ea2889b455287dc7745c875ef7458a91bc29e
Kiryn Preedy <kiryn.preedy@gmail.com>
2025-11-10 14:45:05 -0600

    cw-12.py

--- cw-12.py
+++ cw-12.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
-top = tk()
+top = tk.Tk()
 top.geometry("700x700")
 
-add = tk.Text(width=35, height=2)
+add = tk.Entry(top, width=35)
 add.place(x=100, y=100)
 
 class Queue:
@@ -12,9 +12,17 @@ class Queue:
 
     def enqueue(self):
         valueTxt = tk.Text(top,width = 35, height = 2)
-        x = valueTxt.get(1.0, "end-1c")
+        valueTxt.place(x=100, y=100)
 
-        self.element.append(input("Enter a Value:"))
+        def add_value():
+            x = valueTxt.get(1.0, "end-1c").strip()
+            if x:
+                self.element.append(x)
+                valueTxt.destroy()
+                tk.Button.destroy()
+
+        Button = tk.Button(top, text="add", width=10, height=2, command=add_value)
+        Button.place(x=400, y=95)
 
     def dequeue(self):
         self.element.remove(0)
@@ -23,18 +31,19 @@ class Queue:
         print("Elements in Queue:")
         for i in self.element:
             print(i)
+        print()
 
 
-B1 = tk.Button(top, text="Create Queue", width=10, height=5, command=lambda: show("1"))
+B1 = tk.Button(top, text="Create Queue", width=10, height=5)
 B1.place(x=100, y=150)
 
-B2 = tk.Button(top, text="Enqueue", width=10, height=5, command=lambda: show("1"))
+B2 = tk.Button(top, text="Enqueue", width=10, height=5)
 B2.place(x=100, y=150)
 
-B3 = tk.Button(top, text="Dequeue", width=10, height=5, command=lambda: show("1"))
+B3 = tk.Button(top, text="Dequeue", width=10, height=5)
 B3.place(x=100, y=150)
 
-B4 = tk.Button(top, text="Display Queue", width=10, height=5, command=lambda: show("1"))
+B4 = tk.Button(top, text="Display Queue", width=10, height=5)
 B4.place(x=100, y=150)
 
 #Main
